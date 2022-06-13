8f0ff7dc2ab05f92c39c036a591457a9a6b8a984
Kinan Mohrez <kinanmohrez2015@gmail.com>
2022-06-13 16:31:41 +0200

    add and delete

---
+++ script.js
@@ -0,0 +1,43 @@
+
+// Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close");
+
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+function addTask() {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("input-text").value;
+  console.log('halooooooooooooooooooooooo');
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("todo-list").appendChild(li);
+  }
+  document.getElementById("input-text").value = "";
+
+    let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+
+  
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+
+
